26af7145dd5feb100581d841edcba117993d694d
WoodyFullStack <c24534926@gmail.com>
2022-07-23 18:09:51 +0300

    variables and concatinations

--- my-python-project/main.py
+++ my-python-project/main.py
@@ -1,1 +1,5 @@
-print(1)
+calculation_to_seconds = 24 * 60 * 60
+calculation_to_minutes = 24 * 60
+calculation_to_hours = 24
+name_of_unit = "seconds"
+print(f"20 days are {20 * calculation_to_seconds} {name_of_unit}")
