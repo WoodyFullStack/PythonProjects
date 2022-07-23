dc1f30ce06aa05476a957ae98bee1e9e127aa56c
WoodyFullStack <c24534926@gmail.com>
2022-07-23 20:49:44 +0300

    Deleted call a function from daysFunctions

--- my-python-project/daysFunctions.py
+++ my-python-project/daysFunctions.py
@@ -5,5 +5,3 @@ def days_in_units(days,unitstype):
         return print(f"{days} days are {days * 24 * 60} {unitstype}")
     if unitstype == "seconds":
         return print(f"{days} days are {days * 24 * 60 * 60} {unitstype}")
-
-days_in_units(20,"seconds")
