d4263117373e122da18426e43bebc4614e8b49b4
WoodyFullStack <c24534926@gmail.com>
2022-07-23 20:50:19 +0300

    do import a function from another file in project

--- my-python-project/main.py
+++ my-python-project/main.py
@@ -1,9 +1,3 @@
-def days_in_units(days,unitstype):
-    if unitstype == "hours":
-        return print(f"{days} days are {days * 24} {unitstype}")
-    if unitstype == "minutes":
-        return print(f"{days} days are {days * 24 * 60} {unitstype}")
-    if unitstype == "seconds":
-        return print(f"{days} days are {days * 24 * 60 * 60} {unitstype}")
+import daysFunctions as ds
 
-days_in_units(20,"seconds")
+ds.days_in_units(21,"seconds")
