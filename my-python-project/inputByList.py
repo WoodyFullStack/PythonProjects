9f1dac6e15aca03025bc024d797624e0f7cfb77d
WoodyFullStack <c24534926@gmail.com>
2022-07-27 23:34:14 +0300

    refactoring for loop

--- my-python-project/inputByList.py
+++ my-python-project/inputByList.py
@@ -4,9 +4,8 @@ import daysFunctions as DaysFunction
 # added an input with converting it to a list, to count days in hours by DaysFunction function from other file.
 def list_of_days():
     days_count_input = input("input a count of days to convert them to hours, with spaces between them\n")
-    days_count_input = list(days_count_input.split())
     wrong_inputs = []
-    for x in days_count_input:
+    for x in days_count_input.split():
         # just trying to convert it to integer, to proper count it in hours
         try:
             x = int(x)
