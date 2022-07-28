415aeacac522d3f0d70b0404a9d304fbcb6038a9
WoodyFullStack <c24534926@gmail.com>
2022-07-28 23:43:13 +0300

    added try/except block

--- my-python-project/inputByDict.py
+++ my-python-project/inputByDict.py
@@ -2,10 +2,11 @@ import daysFunctions as DaysFunctions
 
 
 def dict_of_days():
-    days_in_dict = {"days": int(input("Please write a count of days: ")),
+    days_in_dict = {"days": input("Please write a count of days: "),
                     "type": input("Please write a type to what convert: ").lower()}
-    if days_in_dict["days"] > 0:
-        if days_in_dict.get("type") == "Hours" or "minutes" or "seconds":
-            DaysFunctions.days_in_units(int(days_in_dict.get("days")), days_in_dict.get("type"))
-    else:
+    try:
+        if int(days_in_dict["days"]) > 0:
+            if days_in_dict.get("type") == "hours" or "minutes" or "seconds":
+                DaysFunctions.days_in_units(int(days_in_dict["days"]), days_in_dict["type"])
+    except ValueError:
         print("You entered something wrong")
