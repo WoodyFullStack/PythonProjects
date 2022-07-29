17abf5874e26502e67f1d3584f752943bc9e3cae
WoodyFullStack <c24534926@gmail.com>
2022-07-29 22:19:22 +0300

    deadline project added

---
+++ my-python-project/time-till-deadline/time-till-deadline.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+goal = input("Enter your goal with a deadline separated by colon\n").split(":")
+today = datetime.date(datetime.today())
+deadline_date = ""
+try:
+    deadline_date = datetime.date(datetime.strptime(goal[1], "%d.%m.%Y"))
+except ValueError:
+    print("It's not a date")
+    exit()
+if deadline_date < today:
+    print(f"You can't bring back the past. You won't done you goal {(deadline_date-today).days} days ago...")
+else:
+    print(f"Dear user! Time remaining for you goal: {goal[0]} is {(deadline_date-today).days} days.")
