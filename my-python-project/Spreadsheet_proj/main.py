93517e409d796d5aaa8f3b16efcf800c4c8dae50
WoodyFullStack <c24534926@gmail.com>
2022-07-31 01:20:06 +0300

    Added autocalculations with spreadsheets

--- my-python-project/Spreadsheet_proj/main.py
+++ my-python-project/Spreadsheet_proj/main.py
@@ -1,18 +1,18 @@
 import openpyxl
 
-book_path = "Pricebook.xlsx"
-book_obj = openpyxl.load_workbook(book_path)
-sheet_obj = book_obj.active
-max_row = sheet_obj.max_row
-max_column = sheet_obj.max_column
+book_path = "inventory.xlsx"
+book = openpyxl.load_workbook(book_path)
+sheet = book.active
+max_row = sheet.max_row
+max_column = sheet.max_column
 
-# How much products each company have - new company can be added to Excel - it will count. Not only 3.
+# How many products each company have - new company can be added to Excel - it will count. Not only 3.
 company_list = []
 company_list_count = []
 result = {}
 
 for x in range(2, max_row+1):
-    company_list.append(sheet_obj.cell(row=x, column=4).value)
+    company_list.append(sheet.cell(x, 4).value)
 company_list_unique = list(set(company_list))
 for x in company_list_unique:
     counter = 0
@@ -20,15 +20,15 @@ for x in company_list_unique:
         if y == x:
             counter = counter+1
     company_list_count.append(counter)
-for x in range (0, len(company_list_unique)):
+for x in range(0, len(company_list_unique)):
     result[company_list_unique[x]] = company_list_count[x]
 print(result)
 
-# how much products with Inventory less 1000
+# how many products with Inventory less 10
 invent_less_than = {}
 for x in range(2, max_row+1):
-    if sheet_obj.cell(row=x, column=2).value < 1000:
-        invent_less_than[sheet_obj.cell(row=x, column=1).value] = sheet_obj.cell(row=x, column=2).value
+    if sheet.cell(x, 2).value < 10:
+        invent_less_than[sheet.cell(x, 1).value] = sheet.cell(x, 2).value
 print(invent_less_than)
 
 # Inventory value of each company
@@ -36,15 +36,15 @@ company_list_value = []
 for company in company_list_unique:
     counter = 0
     for x in range(2, max_row+1):
-        if sheet_obj.cell(row=x, column=4).value == company:
-            counter = counter+(sheet_obj.cell(row=x, column=2)).value*(sheet_obj.cell(row=x, column=3)).value
+        if sheet.cell(x, 4).value == company:
+            counter = counter + (sheet.cell(x, 2)).value * (sheet.cell(x, 3)).value
     company_list_value.append(counter)
 for x in range(0, len(company_list_unique)):
     result[company_list_unique[x]] = company_list_value[x]
 print(result)
 
 # Write Value for each product
-sheet_obj.cell(row=1, column=max_column+1).value = "Product value"
+sheet.cell(1, max_column + 1).value = "Product value"
 for x in range(2, max_row+1):
-    sheet_obj.cell(row=x, column=max_column+1).value = (sheet_obj.cell(row=x, column=2)).value/(sheet_obj.cell(row=x, column=3)).value
-book_obj.save("Pricebook2.xlsx")
+    sheet.cell(x, max_column + 1).value = (sheet.cell(x, 2)).value * (sheet.cell(x, 3)).value
+book.save("inventory2.xlsx")
