7461d0289fa251047829d8828594041db4bc0e56
WoodyFullStack <c24534926@gmail.com>
2022-08-05 01:17:02 +0300

    correct to PIP format

--- SeleniumLearn/main.py
+++ SeleniumLearn/main.py
@@ -6,12 +6,12 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://selenium-python.readthedocs.io/navigating.html")
 assert "Navigating" in driver.title
-#elem = driver.find_element(By.NAME, "q")
-#print(elem.click())
-#elem.clear()
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.ENTER)
-#assert "No results found." not in driver.page_source
+# elem = driver.find_element(By.NAME, "q")
+# print(elem.click())
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.ENTER)
+# assert "No results found." not in driver.page_source
 driver.close()
 for cookie in driver.get_cookies():
     print(cookie)
