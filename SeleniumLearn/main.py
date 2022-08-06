9abb3f3379d17a247b455571b56e03f6dd55da09
WoodyFullStack <c24534926@gmail.com>
2022-08-06 18:57:26 +0300

    added pytest cleared a bit added "feature" with closing driver

--- SeleniumLearn/main.py
+++ SeleniumLearn/main.py
@@ -5,23 +5,28 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://netmonet.co/")
-# assert "abra-cadabra" in driver.title
-img_elem = driver.find_element(By.XPATH, '//img[@src="https://storage.yandexcloud.net/landing-assets/stats/logos'
-                                         '/logo_4hands.svg"]')
-print(img_elem.get_attribute('style'))
-assert "opacity: 1" in img_elem.get_attribute("style")
-btn_elem = driver.find_element(By.XPATH, '//input[@type="checkbox"]')
-btn_elem.click()
-print(img_elem.get_attribute('style'))
-assert "opacity: 0.1" in img_elem.get_attribute("style")
+VC_SRC = "https://storage.yandexcloud.net/landing-assets/stats/logos/logo_vc.svg"
+img_elem = driver.find_element(By.XPATH, f'//img[@src="{VC_SRC}"]')
+btn_elem = driver.find_elements(By.XPATH, '//input[@type="checkbox"]')[1]
+
+def test_opacity():
+    # assert "abra-cadabra" in driver.title
+
+    assert "opacity: 1" in img_elem.get_attribute("style")
+def test_2():
+    btn_elem.click()
+    assert "opacity: 0.1" in img_elem.get_attribute("style")
 
 
-"""print(elem.click())
+def test_close_chrome():
+    driver.close()
+
+
+"""
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.ENTER)
 assert "No results found." not in driver.page_source
-driver.close()
 for cookie in driver.get_cookies():
 print(cookie)
 """
