6f1af9393edbb6d51feadd7b23dedb863ec5322c
WoodyFullStack <c24534926@gmail.com>
2022-08-06 13:40:34 +0300

    Checking netmonet.co

--- SeleniumLearn/main.py
+++ SeleniumLearn/main.py
@@ -1,17 +1,26 @@
-from selenium import *
+"""This module does blah blah."""
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("https://selenium-python.readthedocs.io/navigating.html")
-assert "Navigating" in driver.title
-# elem = driver.find_element(By.NAME, "q")
-# print(elem.click())
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.ENTER)
-# assert "No results found." not in driver.page_source
+driver.get("https://netmonet.co/")
+#assert "Чаевые картой по QR-коду" in driver.title
+img_elem = driver.find_element(By.XPATH, '//img[@src="https://storage.yandexcloud.net/landing-assets/stats/logos/logo_4hands.svg"]')
+print(img_elem.get_attribute('style'))
+assert "opacity: 1" in img_elem.get_attribute("style")
+btn_elem = driver.find_element(By.XPATH, '//input[@type="checkbox"]')
+btn_elem.click()
+print(img_elem.get_attribute('style'))
+assert "opacity: 0.1" in img_elem.get_attribute("style")
+
+
+"""print(elem.click())
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.ENTER)
+assert "No results found." not in driver.page_source
 driver.close()
 for cookie in driver.get_cookies():
-    print(cookie)
+print(cookie)
+"""
