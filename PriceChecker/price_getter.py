bb5d926f70b93068a067f1f833eeedc5c58c189d
WoodyFullStack <c24534926@gmail.com>
2022-08-16 00:26:09 +0300

    cleaning code, return dictionary with data

--- PriceChecker/price_getter.py
+++ PriceChecker/price_getter.py
@@ -9,6 +9,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+driver = webdriver.Chrome(options=options)
+price_locator = '//div[@class="product-buy__price"]'
+desc_locator = '//h1[@class="product-card-top__title"]'
+
 
 def get_list_of_items(filepath):
     """Just get a list of URLs of the items """
@@ -21,19 +28,18 @@ def get_list_of_items(filepath):
 def get_desc_and_prices():
     """
     Get prices from URLs
-    :return: nothing ATM
+    Do it silently by using --headless argument
+    Use selenium because DNS-shop site doesn't response properly to parse html by beautifullsoup
+    :return: list of items in format {url:[item_description, item_price]}
     """
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    list_of_items = {}
     for item in get_list_of_items('cart.txt'):
         driver.get(item)
-        driver.implicitly_wait(10)
-        price = driver.find_element(By.XPATH, '//div[@class="product-buy__price"]')
-        desc = driver.find_element(By.XPATH, '//div[@class="product-card-top__specs"]')
-        print(f"Товар {desc.text} стоит {price.text}ублей")
+        driver.implicitly_wait(5)
+        price = driver.find_element(By.XPATH, price_locator)
+        desc = driver.find_element(By.XPATH, desc_locator)
+        list_of_items[item] = [desc.text, price.text[0:-2]]
     driver.close()
+    return list_of_items
 
-
-get_desc_and_prices()
+print(get_desc_and_prices())
