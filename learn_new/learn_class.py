920ccf04d0128374f83b6d86bd1eba8d25a4f8c5
bitbucket_reader <a@a.com>
2022-08-29 13:16:06 +0300

    add parent class

--- learn_new/learn_class.py
+++ learn_new/learn_class.py
@@ -1,5 +1,9 @@
+class SuperParentClass:
+    def print_hello(self, argument):
+        print(argument)
 
-class my_first_class:
+
+class my_first_class(SuperParentClass):
     """This class about public and private attributes"""
     x = 1
     _y = 2
@@ -8,22 +12,20 @@ class my_first_class:
     def get_z(self):
         print(self.__z)
 
-    def set_z(self, z):
-        __z = z
-
+    @property
+    def y(self):
+        return self._y
 
 
 my_first_object = my_first_class()
 # print public class attribute
 print(my_first_object.x)
 # print protected class attribute
-print(my_first_object._y)
+print(my_first_object.y)
 # try to print private class attribute - it's impossible
 try:
     print(my_first_object.z)
 except AttributeError:
     print("AttributeError raised. You can't print 'z', because it's private")
 
-my_first_object.get_z()
-my_first_object.set_z(4)
-my_first_object.get_z()
+my_first_object.print_hello("Print method called from SuperParentClass")
